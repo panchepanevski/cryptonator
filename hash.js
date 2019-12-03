1c62d344ecbb99ae37189738d7f8136eba6505db
panchepanevski <pance.panevski@gmail.com>
2019-12-03 11:48:26 +0100

    add all for readline

--- hash.js
+++ hash.js
@@ -1,25 +1,10 @@
 const fs = require("fs");
-const readline = require("readline");
-
+const { askForPassword } = require("./utils/input");
 const { hashPassword } = require("./utils/crypto");
 
-const readlineInterface = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-readlineInterface.question("Please enter new master password:", password => {
-  // Add line break
-  readlineInterface.output.write("\n");
-
+askForPassword().then(password => {
   const hashedPassword = hashPassword(password);
   fs.writeFileSync(".password", hashedPassword);
 
   console.log("New password saved");
-  readlineInterface.close();
 });
-
-// Override default output to hide password
-readlineInterface._writeToOutput = function() {
-  readlineInterface.output.write("");
-};
